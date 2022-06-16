0edb69ffc61ab358847910c4de2ab45619d749c6
ccrismota <ccrismota@gmail.com>
2022-06-16 13:49:29 -0300

    start project-dependencias

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'soul-digital-c15f6',
+    appId: '1:358569400221:web:ea5f321649ac68888f8174',
+    storageBucket: 'soul-digital-c15f6.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDFh95qVpakS2BpH_TIqHzmPaI-6woTqm8',
+    authDomain: 'soul-digital-c15f6.firebaseapp.com',
+    messagingSenderId: '358569400221',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'soul-digital-c15f6',
+    appId: '1:358569400221:web:ea5f321649ac68888f8174',
+    storageBucket: 'soul-digital-c15f6.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDFh95qVpakS2BpH_TIqHzmPaI-6woTqm8',
+    authDomain: 'soul-digital-c15f6.firebaseapp.com',
+    messagingSenderId: '358569400221',
+  },
   production: false
 };
 
